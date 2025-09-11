facf5eb4540b2712889bd038fa85e57f2abfef6e
gglennd <grdimaano18@gmail.com>
2025-09-11 13:33:21 +0800

    Implement some vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,12 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
+import packageConfig from "./package.json";
+
 export default defineConfig({
+  define: {
+    __APP_VERSION__: JSON.stringify(packageConfig.version),
+  },
   plugins: [
     tsConfigPaths(),
     tanstackStart({ customViteReactPlugin: true }),
